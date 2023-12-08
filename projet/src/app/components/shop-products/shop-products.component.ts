1352b85439c37d929d458fad4e6edb75afb57ae6
Wubpooz <mathieu.waharte@u-psud.fr>
2023-12-08 13:19:06 +0000

    push

--- projet/src/app/components/shop-products/shop-products.component.ts
+++ projet/src/app/components/shop-products/shop-products.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'shop-products',
@@ -10,7 +12,29 @@ import { gsap } from 'gsap';
   styleUrl: './shop-products.component.scss'
 })
 export class ShopProductsComponent {
-  //https://codepen.io/sunshinetainted/pen/vYeGVNd
-  
 
+  //https://codepen.io/sunshinetainted/pen/vYeGVNd
+  //https://codepen.io/NewbieRuby/pen/rNYejNb
+
+
+  ngOnInit() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.set(".massiveImage", {backgroundImage: `url(https://source.unsplash.com/random/${innerWidth * 3}x${innerHeight})`})
+
+    gsap.to(".massiveImage", {
+      xPercent: -100, 
+      x: () => innerWidth,
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".massiveImage",
+        start: "top top",
+        end: () => innerWidth * 3,
+        scrub: true,
+        pin: true,
+        invalidateOnRefresh: true,
+        anticipatePin: 1
+      }
+    });
+  }
 }
