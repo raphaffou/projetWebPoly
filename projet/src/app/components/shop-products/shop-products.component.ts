a3c9d6291a36a12c945b9ef82527e5e65888779f
Wubpooz <mathieu.waharte@u-psud.fr>
2023-12-08 15:41:38 +0000

    scrolltriggers

--- projet/src/app/components/shop-products/shop-products.component.ts
+++ projet/src/app/components/shop-products/shop-products.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
-
+import { TextPlugin } from 'gsap/TextPlugin';
 @Component({
   selector: 'shop-products',
   standalone: true,
@@ -20,21 +19,55 @@ export class ShopProductsComponent {
   ngOnInit() {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.set(".massiveImage", {backgroundImage: `url(https://source.unsplash.com/random/${innerWidth * 3}x${innerHeight})`})
+    let products = gsap.utils.toArray(".product");
 
-    gsap.to(".massiveImage", {
-      xPercent: -100, 
+    gsap.to("#caroussel", {
+      xPercent: -100 * (products.length - 1),
       x: () => innerWidth,
       ease: "none",
       scrollTrigger: {
-        trigger: ".massiveImage",
+        trigger: "#caroussel",
         start: "top top",
         end: () => innerWidth * 3,
-        scrub: true,
+        scrub: 1,
         pin: true,
+        snap: 1 / (products.length - 1),
         invalidateOnRefresh: true,
         anticipatePin: 1
       }
     });
+
+
+    const text_sections = gsap.utils.toArray('.text');
+    console.warn(text_sections);
+
+    text_sections.forEach((section:any) => {
+      gsap.set(section, { y: 30, opacity: 0});
+
+      ScrollTrigger.create({
+        trigger: section,
+        markers : true,
+        start: () => `top-=${gsap.getProperty(section, 'y')} bottom-=100`,
+        end: () => `+=${section.clientHeight}`,
+        onEnter: () => {
+          gsap.to(section, {y: 0, opacity: 1});
+        },
+        onLeaveBack: () => {
+          gsap.to(section, {y: 30, opacity: 0});
+        }
+      })});
+
   }
 }
+
+/*
+xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  */
