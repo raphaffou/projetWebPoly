b818d4241e191afba8d3e25ab73566947141f6b1
Wubpooz <mathieu.waharte@u-psud.fr>
2024-02-15 17:48:04 +0000

    Product pages mockup

--- projet/src/app/product-page/product-page.component.ts
+++ projet/src/app/product-page/product-page.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { products } from '../products';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.scss']
 })
@@ -17,12 +18,13 @@ export class ProductPageComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.product = products.find(params['id']);
+      this.product = products.find(product => product.id == params['id']);
     });
   }
 
   //add to cart
-  addToCart(product: any) {
+  addToCart() {
     console.log('Your product has been added to the cart!');
+    //add this.product
   }
 }
